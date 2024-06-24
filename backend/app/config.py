8bfce5e2a2daf944df904b754cb3db6f2911f607
carson <108508786+cyanblock@users.noreply.github.com>
2024-06-24 13:24:46 -0500

    add cx ID

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,6 +1,6 @@
 class Config:
     GOOGLE_API_KEY = 'AIzaSyDa83ukRR4dlqykb_5NmxRm3o-BKj6_zkY'
-    GOOGLE_CX = 'our_cx_id' # needs adjustment
+    GOOGLE_CX = '674dd5606f7914a34' # needs adjustment
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'username' # needs adjustment
     MYSQL_PASSWORD = 'password' # needs adjustment
