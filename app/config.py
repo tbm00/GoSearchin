3bb0a52c6e8ccd9351f433fb078533d01cc72f0f
carson <108508786+cyanblock@users.noreply.github.com>
2024-06-27 15:27:43 -0500

    get rid of keys

--- app/config.py
+++ app/config.py
@@ -4,20 +4,20 @@ import os
 
 class Config:
     # Google Search API
-    GOOGLE_API_KEY = 'AIzaSyDa83ukRR4dlqykb_5NmxRm3o-BKj6_zkY'
-    GOOGLE_CX = '674dd5606f7914a34'
+    GOOGLE_API_KEY = ''
+    GOOGLE_CX = ''
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'key'
 
     # MySQL: User Database
-    USER_DB_HOST = '194.104.156.210'
-    USER_DB_PORT = '3306'
-    USER_DB_USER = 'u30529_i8EvvPQCAB'
-    USER_DB_PASS = '1BrMMkcomapI9zdW=aeuBMMl'
-    USER_DB_NAME = 's30529_userDB'
+    USER_DB_HOST = ''
+    USER_DB_PORT = ''
+    USER_DB_USER = ''
+    USER_DB_PASS = ''
+    USER_DB_NAME = ''
 
     # MySQL: Knowledgebase Database
-    #KNOW_DB_HOST = 'localhost'
-    #KNOW_DB_PORT = '3306'
-    #KNOW_DB_USER = 'bassholes'
-    #KNOW_DB_PASS = 'password'
-    #KNOW_DB_NAME = 'knowledgeDB'
+    #KNOW_DB_HOST = ''
+    #KNOW_DB_PORT = ''
+    #KNOW_DB_USER = ''
+    #KNOW_DB_PASS = ''
+    #KNOW_DB_NAME = ''
